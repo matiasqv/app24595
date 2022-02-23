75d38958b87dbacc396f0060dd1690520a4f9a91
matiasqv <matiasqv@gmail.com>
2022-02-23 11:40:50 -0300

    PEF

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 
 import './App.css'
+import { useState } from 'react'
 import NavBar from './components/NavBar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailConteiner from './components/ItemDetailConteiner/ItemDetailConteiner'
 
 
 
-function App() {
+const App = () => {
+  const [route, setRoute] = useState('list')
+
   return (
     <div className="App">
+      <div>
+        <button onClick = {() => setRoute ('list')}>list</button>
+        <button onClick = {() => setRoute ('detail')}>Detail</button>
+      </div>
       {/* este title  y color viaja a NavBar por las props */}
       <NavBar title="e-miTienda" color="purple" />
-      <ItemListContainer greeting="HOLA MUNDO" color="purple" />
-      <ItemDetailConteiner />
+      {/*{route === 'list' ? <ItemListContainer/> : null}  // Forma de escribirlo en forma ternaria*/}
+      {route === 'list' && <ItemListContainer greeting="HOLA MUNDO" color="purple" />}
+      {route === 'detail' && <ItemDetailConteiner />}
     </div>
   );
 }
