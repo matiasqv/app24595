b4bd0fbff9d4afcb63727ebb9af81c09fb107fc4
matiasqv <matiasqv@gmail.com>
2022-03-02 10:34:51 -0300

    cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 
 import '../CartWidget/CartWidget.css'
+import { useState, useContext } from 'react'
+import { MyContext } from '../../App'
 
 
 /* VER BIEN AQUI LOS CLASSNAME Y ESTILOS, LAS IMPORTACIONES */
@@ -8,11 +10,14 @@ const cantidad = 10
 
 const Cart = () => {
 
+    const  {cart, setCart} = useContext(MyContext)
+    console.log(cart)
+
     return (
         <div className='ItemDetailContainer'>
             <h1>Carrito de Compras</h1>
             <img src={'../images/carrito.png'} className="Carrito" />
-            <p className="Numero">{cantidad}</p>
+            <p className="Numero">{cart}</p>
         </div>
     )
 }
