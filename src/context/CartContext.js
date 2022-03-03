a2f4359092a8f8dd3e3fc9d4da4ff4a03e6e7e25
matiasqv <matiasqv@gmail.com>
2022-03-02 21:50:17 -0300

    context

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,27 +3,34 @@ import { createContext, useState } from "react"
 
 const Context = createContext()
 
-export const CartContextProvider =({children}) => {
+export const CartContextProvider = ({ children }) => {
 
-    const[cart, setCart] = useState([])
+    const [cart, setCart] = useState([])
 
 
     console.log(cart)
 
-    const productToAdd = (productDetail,
-        count) => {
-        setCart([...cart, {productDetail, count}])
-
+    const productToAdd = (productDetail, count) => {
+        if (isInCart(productDetail.id)) {
+                alert('hay que sumar al carrito');
+        } else {
+            setCart([...cart, { ...productDetail, count }])
+        }
         console.log(productDetail, count)
-
     }
+
+    const isInCart = (id) => {
+        const validacion = cart.some((product) => product.id === id)
+        return validacion
+    }
+
 
 
     return (
         <Context.Provider value={{
             cart,
             productToAdd
-            }}>
+        }}>
             {children}
         </Context.Provider>
     )
